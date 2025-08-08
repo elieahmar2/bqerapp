3bdd651088e6e06490bd9570db03719518a91676
elieahmar2 <150794762+elieahmar2@users.noreply.github.com>
2025-08-08 03:15:53 +0300

    Update SplashVideoActivity.java

--- app/src/main/java/com/bqerzala/online/SplashVideoActivity.java
+++ app/src/main/java/com/bqerzala/online/SplashVideoActivity.java
@@ -1,16 +1,40 @@
 package com.bqerzala.online;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
+import android.media.MediaPlayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashVideoActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // جرب تفتح MainActivity فورًا بدون أي فيديو
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+        // إنشاء VideoView برمجيًا أو يمكن تعريفه في xml
+        videoView = new VideoView(this);
+        setContentView(videoView);
+
+        // تعريف مسار الفيديو داخل الـ raw
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video);
+
+        // تعيين مصدر الفيديو للـ VideoView
+        videoView.setVideoURI(videoUri);
+
+        // تشغيل الفيديو تلقائيًا
+        videoView.start();
+
+        // الاستماع لانتهاء الفيديو للانتقال إلى MainActivity
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                startActivity(new Intent(SplashVideoActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
